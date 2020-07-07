7b414b37b667085e84467028d8d42a180dede9dc
Sergey Mikheev <mikheevs11@gmail.com>
2020-07-07 18:38:24 +0300

    Added test json

--- app/components/DashboardPage.jsx
+++ app/components/DashboardPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+const EXAMPLE_JSON = {
+  "var1": "value1",
+  "array": ["1", "2", "3"],
+  "object": {
+    "var2": "value2"
+  }
+}
+
 const DashboardPage = props => (
   <article>
     <Helmet>
       <title>{props.title}</title>
     </Helmet>
     <div>
-      v2
       Dashboard
+      <div>{JSON.stringify(EXAMPLE_JSON)}</div>
     </div>
   </article>
 )
