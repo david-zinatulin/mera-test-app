d55c24934c5edb5c08b35540338c0d37f34e3d09
Sergey Mikheev <mikheevs11@gmail.com>
2020-07-07 20:11:56 +0300

    Added display tree of first level json

--- app/components/DashboardPage.jsx
+++ app/components/DashboardPage.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import data from "../assets/data";
+import { element } from 'prop-types';
+
+const displayJson = () => {
+  const dataKeys = Object.keys(data);
+  const dataValues = Object.values(data);
+  const newArray = [];
+  dataKeys.forEach((element, index) => {
+    const newEl = element + ":" + (typeof (dataValues[index]) === 'object' ? "{...}" : dataValues[index]);
+    newArray.push(newEl);
+  });
+  return newArray;
+}
 
 const DashboardPage = props => (
   <article>
@@ -8,7 +20,15 @@ const DashboardPage = props => (
       <title>{props.title}</title>
     </Helmet>
     <div>
-      <code lang="javascript">{data.glossary.title}</code>
+      <code lang="javascript">
+        {/* {Object.keys(data).map(key => <div>{key}</div>)}
+        {Object.values(data).map(value =>
+          typeof (value) === 'object' ? <div>{"{...}"}</div> : <div>{value}</div>
+        )} */}
+        <div>{"{"}</div>
+        {displayJson().map(elem => <div style={{ marginLeft: 30 + "px" }}>{elem}</div>)}
+        <div>{"}"}</div>
+      </code>
     </div>
   </article>
 )
