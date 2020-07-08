3a2394f8775be5f6361be41e991467fa61d6d750
Sergey Mikheev <mikheevs11@gmail.com>
2020-07-08 03:36:39 +0300

    Fixed first version of displaying

--- app/components/DashboardPage.jsx
+++ app/components/DashboardPage.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import data from "../assets/data";
-import { element } from 'prop-types';
 
-const displayJson = () => {
+const prepareJson = (data) => {
   const dataKeys = Object.keys(data);
   const dataValues = Object.values(data);
-  const newArray = [];
-  dataKeys.forEach((element, index) => {
-    const newEl = element + ":" + (typeof (dataValues[index]) === 'object' ? "{...}" : dataValues[index]);
-    newArray.push(newEl);
+  const newArray = dataKeys.map((element, index) => {
+    const { style, value } = checkValueForStyle(dataValues[index])
+    return {
+      __html: "<span style=\"color: green\">\"" + element + "\"</span>" +
+        "<span>: </span>" +
+        "<span style=\"" + style + "\">" + value + "</span>"
+    }
   });
   return newArray;
+}
+const checkValueForStyle = (value) => {
+  switch (typeof (value)) {
+    case 'string':
+      return { style: "color: purple", value: "\"" + value + "\"" };
+    case 'number':
+      return { style: "color: #24D616", value }
+    default:
+      if (Array.isArray(value)) {
+        return { style: "color: orange", value: "[ " + value + " ]" };
+      } else {
+        //TODO: correctly display the object
+        //inserts __html objects into __html objects
+        return {
+          style: "color: #159A9E", value: prepareJson(value)
+        }
+      }
+  }
+}
+
+const displayObject = (data) => {
+  const jsonArray = prepareJson(data);
+  return jsonArray.map((elem, index) => {
+    const base = <div style={{ marginLeft: 30 + "px" }} dangerouslySetInnerHTML={elem}></div>;
+    if (index === 0) {
+      let array = [];
+      array.push(<div>{"{"}</div>);
+      array.push(base);
+      return array;
+    }
+    if (index === (jsonArray.length - 1)) {
+      let array = [];
+      array.push(base);
+      array.push(<div>{"}"}</div>)
+      return array;
+    }
+    return base;
+  })
 }
 
 const DashboardPage = props => (
@@ -20,14 +60,8 @@ const DashboardPage = props => (
       <title>{props.title}</title>
     </Helmet>
     <div>
-      <code lang="javascript">
-        {/* {Object.keys(data).map(key => <div>{key}</div>)}
-        {Object.values(data).map(value =>
-          typeof (value) === 'object' ? <div>{"{...}"}</div> : <div>{value}</div>
-        )} */}
-        <div>{"{"}</div>
-        {displayJson().map(elem => <div style={{ marginLeft: 30 + "px" }}>{elem}</div>)}
-        <div>{"}"}</div>
+      <code>
+        {displayObject(data)}
       </code>
     </div>
   </article>
