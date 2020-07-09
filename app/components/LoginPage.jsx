e057ad1ce4a0739a5401f8a163c7831a68d1196f
Vladislav <vladalekseev9@yandex.ru>
2020-07-09 19:14:05 +0300

    :wrench: add css modules config in webpack

--- app/components/LoginPage.jsx
+++ app/components/LoginPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-
+import style from './LoginPage.css';
 const LoginPage = props => (
   <article>
     <Helmet>
       <title>{props.title}</title>
     </Helmet>
-    <div>
+    <div className={style.loginPage}>
       Login
     </div>
   </article>
