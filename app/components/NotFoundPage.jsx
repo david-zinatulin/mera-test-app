1fba76a75ce84fe81f32f2c732780b1c80a2df6f
David Zinatulin <david.zinatulin@mera.com>
2020-07-10 19:08:32 +0300

    config lint and prettier

--- app/components/NotFoundPage.jsx
+++ app/components/NotFoundPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const NotFoundPage = props => (
-  <article>
-    <Helmet>
-      <title>{props.title}</title>
-    </Helmet>
-    <div>
-      404
-    </div>
-  </article>
-)
+const NotFoundPage = ({ title }) => (
+    <article>
+        <Helmet>
+            <title>{title}</title>
+            <span>sfsdf</span>
+            <span>sfsdf</span>
+            <span>sdfsdfs</span>
+        </Helmet>
+        <div>404</div>
+    </article>
+);
 
 export default NotFoundPage;
