689f9fca884313eda418983163be12822c711cee
David Zinatulin <david.zinatulin@mera.com>
2020-07-10 19:08:32 +0300

    config lint and prettier

--- app/components/DashboardPage.jsx
+++ app/components/DashboardPage.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 const DashboardPage = props => (
-  <article>
-    <Helmet>
-      <title>{props.title}</title>
-    </Helmet>
-    <div>
-      v2
-      Dashboard
-    </div>
-  </article>
-)
+    <article>
+        <Helmet>
+            <title>{props.title}</title>
+        </Helmet>
+        <div>v2 Dashboard</div>
+    </article>
+);
 
 export default DashboardPage;
