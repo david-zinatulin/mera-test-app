b486836e1b69180e94e3999bc3f05cde7fbd9a22
Sergey Mikheev <mikheevs11@gmail.com>
2020-07-10 18:36:42 +0300

    Code refactor

--- app/components/DashboardPage/JsonNode.jsx
+++ app/components/DashboardPage/JsonNode.jsx
@@ -2,22 +2,12 @@ import React, { useRef, useState } from 'react';
 import css from "./DashboardPage.module.css";
 
 const JsonNode = ({ value, element, index, length, className, isObject, valueType }) => {
-    let objectRef = useRef();
-
     const NODE_CLOSED_BUTTON_ICON = "+";
     const NODE_OPENED_BUTTON_ICON = "-";
 
     const [isDisplayed, setIsDisplayed] = useState(true);
-    const [icon, changeIcon] = useState(NODE_OPENED_BUTTON_ICON);
 
     const toggleView = () => {
-        if (isDisplayed) {
-            objectRef.current.style.display = "none";
-            changeIcon(NODE_CLOSED_BUTTON_ICON);
-        } else {
-            objectRef.current.style.display = "";
-            changeIcon(NODE_OPENED_BUTTON_ICON);
-        }
         setIsDisplayed(!isDisplayed);
     }
 
@@ -46,13 +36,20 @@ const JsonNode = ({ value, element, index, length, className, isObject, valueTyp
         <React.Fragment>
             <span className={css.key}>{"\"" + element + "\""}</span>
             <span className={css.separator}>
-                : {checkObjectOrArrayEmpty(value) && isObject && <button className={css.toggleViewButton} onClick={toggleView}>{icon}</button>}
+                :<span> </span>
+                {
+                    checkObjectOrArrayEmpty(value) &&
+                    isObject &&
+                    <button className={css.toggleViewButton} onClick={toggleView}>
+                        {isDisplayed ? NODE_OPENED_BUTTON_ICON : NODE_CLOSED_BUTTON_ICON}
+                    </button>
+                }
             </span>
             <span> </span>
             {!isDisplayed && <span className={valueType === "array" ? css.array : css.object}>{displayShortcutObjectArray(valueType)}</span>}
-            <span className={className} ref={objectRef}>
+            {isDisplayed && <span className={className}>
                 {valueType === "boolean" ? "" + value : value}
-            </span>
+            </span>}
             {index !== length - 1 ? <span className={css.separator}>,</span> : null}
         </React.Fragment>
     );
