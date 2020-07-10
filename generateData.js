f89433b76f0fdb96b227520b283e9e4618a1c585
Nikita Treskov <treskov-1@mail.ru>
2020-07-10 19:56:01 +0300

    Auto stash before merge of "auth" and "origin/auth"

--- generateData.js
+++ generateData.js
@@ -1,5 +1,6 @@
 var faker = require('faker');
 
+<<<<<<< Updated upstream
 var database = { products: [] };
 
 for (var i=1; i<=1000; i++) {
@@ -7,6 +8,15 @@ for (var i=1; i<=1000; i++) {
     id: i,
     name: faker.internet.userName(),
     password: faker.internet.password()  
+=======
+var database = { users: [] };
+
+for (var i=1; i<=10; i++) {
+  database.users.push({
+    id: i,
+    username: faker.internet.userName(),
+    password: faker.internet.password()
+>>>>>>> Stashed changes
   });
 }
 
