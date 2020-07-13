6fbca6c7cc4fbcf8e0062b28d11b56bc961dfed8
Treskov Nikita <treskov-1@mail.ru>
2020-07-13 14:07:21 +0300

    Login html, css and basic handler

--- app/components/LoginPage.jsx
+++ app/components/LoginPage.jsx
@@ -1,19 +1,43 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {connect} from 'react-redux';
 import { Helmet } from 'react-helmet';
+import style from './LoginPage.css';
 
-const LoginPage = () =>{
-  return (
-  <div className="login-Page">
-  <div className="form">    
-    <form className="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-      <p class="message">Not registered? <a href="#">Create an account</a></p>
+function LoginPage (props) {
+
+  const[state,setState]=useState({
+    username:"",
+    password:""
+  });
+
+  const handleChange = (e) => {
+    const {id , value} = e.target   
+    setState(prevState => ({
+        ...prevState,
+        [id] : value
+    }))
+  }
+
+const handleSubmit=()=>{
+  alert('Email address is ' + state.username + ' Password is ' + state.password);            
+}
+
+return(
+  <article>
+    <Helmet>
+      <title>{props.title}</title>
+    </Helmet>
+    <div className={style.loginpage}>
+    <div className={style.form}>
+    <form className={style.loginform}>
+      <input className={style.forminput} value={state.username} onChange={handleChange} id="username" type="text" placeholder="username"/>
+      <input className={style.forminput} value={state.password} onChange={handleChange} id="password" type="password" placeholder="password"/>
+      <button className={style.formbutton} onClick={handleSubmit}>login</button>
+      <p className={style.message}>Not registered? <a href="#">Create an account</a></p>
     </form>
-  </div>
-</div>
+    </div>
+    </div>
+  </article>
   )
 }
 
